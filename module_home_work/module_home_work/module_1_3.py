9064bf67aa6e4c16559ab0e00b4a5d7f34d398a5
Ingwar <86135910+GloryLoryLord@users.noreply.github.com>
2024-10-15 20:14:47 +0300

    1.3 + one
    дополнительное решение

---
+++ module_home_work/module_home_work/module_1_3.py
@@ -0,0 +1,34 @@
+# name = 'Ingwar'
+# print (name)
+#
+# age = 27
+# print(age)
+#
+# age = 16
+# print(age)
+# new_age = 21
+# print (new_age)
+#
+# is_student = True
+# print(is_student)
+#
+# print(new_age - age)
+
+#-------------------------------------
+
+name = str(input('enter name: '))
+print (name)
+
+age = int(input('enter age: '))
+print (age)
+
+age = 16
+print(age)
+
+new_age = int(input('enter new_age: '))
+print (new_age)
+
+is_student = bool(input('enter bool status: '))
+print(is_student)
+
+print(new_age - age)
